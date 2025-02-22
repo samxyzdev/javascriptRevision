a6c91eaa0dd86f0019b64eb4f17179c2626db27a
Sameer Ahmed <samxyzdev@gmail.com>
2025-02-22 16:45:31 +0000

    02_DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 + 3); // code readability shoudl be high
+
+let name = "Sameer";
+let age = 18;
+let isLoggedIN = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value. Its also a dataType
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
