665fd60ebbf2aee0eb0c12936cb456d2cece16fc
Sameer Ahmed <samxyzdev@gmail.com>
2025-02-22 17:05:42 +0000

    03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// DataTypes which convert into
+// "33" => 33
+// "33abd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = true;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// DataTypes which convert into
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber); // => string
+
+// ************************ Operations **************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
