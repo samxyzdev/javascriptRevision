aeae75b3cca8a91cc3bdbfad3966951ca3f46a81
Sameer Ahmed <samxyzdev@gmail.com>
2025-02-22 17:17:42 +0000

    03_conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,4 +30,33 @@ let stringNumber = String(someNumber);
 
 let value = 3;
 let negValue = -value;
-console.log(negValue);
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2); // => 12
+console.log(1 + "2"); // => 12
+console.log("1" + 2 + "2"); // => 122
+console.log(1 + 2 + "2"); // 32
+// |=> Its all happen because of ecmascript guideline
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
