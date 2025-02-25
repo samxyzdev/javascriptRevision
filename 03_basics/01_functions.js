17e04e2983f99fa7509741e0aff12274047edd11
Sameer Ahmed <samxyzdev@gmail.com>
2025-02-25 05:44:36 +0000

    01_function.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -49,3 +49,40 @@ function loginUserMessage(username) {
 
 console.log(loginUserMessage);
 console.log(loginUserMessage("Sameers")); // => Sameers just logged in
+
+function calculateCartPrice(...num1) {
+  return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)); // => [ 200, 400, 500, 2000 ]
+
+function calculateCartPrice1(val1, val2, ...num1) {
+  return num1;
+}
+
+console.log(calculateCartPrice1(200, 400, 500, 2000)); // => [ 500, 2000 ]
+
+const user = {
+  username: "hitesh",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  username: "sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return;
+}
+
+console.log(returnSecondValue(myNewArray));
